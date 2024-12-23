33378a31fa6a5a66fe4db39cd6041dc1efea95c1
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-23 23:07:52 +0530

    Added: button in input

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -8,17 +8,17 @@ interface TodoProps{
 
 const InputField = ({ todo, setTodo, handleAddTodo }: TodoProps) => {
   return (
-    <form className="w-[95%] flex items-center relative" onSubmit={handleAddTodo}>
+    <form className="w-[95%] flex justify-center items-center relative" onSubmit={handleAddTodo}>
       <input
         type="text"
         placeholder="Enter your todo"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
-        className="w-full p-4 pr-16 rounded-sm" // Add padding-right for button space
+        className="w-[85%] p-4 pr-16 rounded-lg shadow-lg outline-none" // Add padding-right for button space
       />
       <button
         type="submit"
-        className="bg-blue-400 text-white absolute right-2 top-1/2 transform -translate-y-1/2 p-2 rounded-full"
+        className="bg-blue-400 text-white absolute right-28 top-1/2 transform -translate-y-1/2 p-2 rounded-full shadow-lg"
       >
         Go
       </button>
