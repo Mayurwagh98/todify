e994f58dd96c25f27b9c69d043dd7e96ae12d56d
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-23 22:14:28 +0530

    Added: heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <>Hello mayur</>;
-}
+const App: React.FC = () => {
+  return <div className="bg-slate-400 w-full h-full">
+      <h1 className="text-teal-400 text-3xl font-bold text-center">Todify</h1>
+    </div>
+};
 
 export default App;
