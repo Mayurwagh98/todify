1bbabc1fea5055ea1b05832e1574eaae80da8e41
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-26 15:56:50 +0530

    Added: edit feature

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -2,6 +2,7 @@ import { FiEdit3 } from "react-icons/fi";
 import { MdDeleteOutline } from "react-icons/md";
 import { IoMdDoneAll } from "react-icons/io";
 import { Todo } from "../models/models";
+import { useEffect, useRef, useState } from "react";
 
 interface props {
   todo: Todo;
@@ -10,8 +11,15 @@ interface props {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editDefaultVal, setEditDefaultVal] = useState<string>(todo.todo);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   const handleComplete = (id: number) => {
-    console.log(todo);
     setTodos(
       todos?.map((todo) =>
         todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
@@ -23,19 +31,41 @@ const SingleTodo = ({ todo, todos, setTodos }: props) => {
     setTodos(todos?.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos?.map((todo) =>
+        todo.id === id ? { ...todo, todo: editDefaultVal } : todo
+      )
+    );
+    setEdit(false);
+  };
+
   return (
     <div className="w-1/2 m-2 p-2 rounded-lg bg-orange-500 flex justify-between items-center shadow-xl">
       <div className="w-1/2">
-        <li
-          className={`m-2 p-2 list-none text-white ${
-            todo.isDone ? "line-through" : ""
-          }`}
-        >
-          {todo.todo}
-        </li>
+        {edit ? (
+          <form onSubmit={(e) => handleEdit(e, todo.id)}>
+            <input
+              type="text"
+              onChange={(e) => setEditDefaultVal(e.target.value)}
+              ref={inputRef}
+              value={editDefaultVal}
+              className="m-2 p-2 list-none text-black outline-none rounded-sm"
+            />
+          </form>
+        ) : (
+          <li
+            className={`m-2 p-2 list-none text-white ${
+              todo.isDone ? "line-through" : ""
+            }`}
+          >
+            {todo.todo}
+          </li>
+        )}
       </div>
       <div className="flex justify-between items-center w-[15%] pr-2">
-        <span>
+        <span onClick={() => setEdit((prev) => !prev)}>
           <FiEdit3 className="text-2xl cursor-pointer" />
         </span>
         <span onClick={() => handleDelete(todo.id)}>
