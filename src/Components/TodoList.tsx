9a5badc3ba82a4000101341d257d7058b65c9521
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-26 17:43:44 +0530

    Added: Dropable

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { Droppable } from "react-beautiful-dnd";
 import { Todo } from "../models/models";
 import SingleTodo from "./SingleTodo";
 
@@ -9,28 +10,45 @@ interface TodosProps {
 const TodoList = ({ todos, setTodos }: TodosProps) => {
   return (
     <div className="flex justify-center items-center mt-2">
-      <div className="w-1/2 m-2 p-2 rounded-md bg-teal-300 shadow-sm">
-        <h2 className="text-center text-xl text-white">Active Tasks</h2>
-        {todos?.map((todo) => (
-          <SingleTodo
-            todo={todo}
-            key={todo.id}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))}
-      </div>
-      <div className="w-1/2 m-2 p-2 rounded-md bg-green-400 shadow-sm">
-        <h2 className="text-center text-xl text-white">Completed Tasks</h2>
-        {todos?.map((todo) => (
-          <SingleTodo
-            todo={todo}
-            key={todo.id}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))}
-      </div>
+      <Droppable droppableId="TodoList">
+        {(provided) => (
+          <div
+            className="w-1/2 m-2 p-2 rounded-md bg-teal-300 shadow-sm"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h2 className="text-center text-xl text-white">Active Tasks</h2>
+            {todos?.map((todo) => (
+              <SingleTodo
+                todo={todo}
+                key={todo.id}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+
+      <Droppable droppableId="RemovedList">
+        {(provided) => (
+          <div
+            className="w-1/2 m-2 p-2 rounded-md bg-green-400 shadow-sm"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h2 className="text-center text-xl text-white">Completed Tasks</h2>
+            {todos?.map((todo) => (
+              <SingleTodo
+                todo={todo}
+                key={todo.id}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
